56206170a2f5d456c26d5bb4c6e12b1b13e03907
Isabell Carrascal <mi.carrascal@uniandes.edu.co>
2021-04-03 10:18:04 -0500

    Cifrado Simetrico y Asimetrico completo

--- src/Criptografia.java
+++ src/Criptografia.java
@@ -22,6 +22,77 @@ public class Criptografia {
 		
 	}
 	
+    public KeyPair generadorLLaves() throws NoSuchAlgorithmException {
+		//Generacion de claves
+		KeyPairGenerator generator = KeyPairGenerator.getInstance(ALGORITMO);
+		generator.initialize(1024);
+		return generator.generateKeyPair();
+	}
 	
+	public SecretKey generadorLLave128() throws NoSuchAlgorithmException {
+		//Generacion de clave SecretKey
+		KeyGenerator generator = KeyGenerator.getInstance("AES");
+		generator.init(128);
+		return generator.generateKey();
+	}
+	
+	
+	public static byte[] cifrarAsimetrico(Key llave, String texto){
+		 byte[] textoCifrado;
+		 
+		 try {
+			 Cipher cifrador = Cipher.getInstance(ALGORITMO);
+			 byte[] textoClaro = texto.getBytes();
+			 
+			 cifrador.init(Cipher.ENCRYPT_MODE, llave);
+			 textoCifrado = cifrador.doFinal(textoClaro);
+			 
+			 return textoCifrado;
+		 }catch(Exception e) {
+			 System.out.println("Exception: " + e.getMessage());
+			 return null;
+		 }
+	}
+	
+	public static byte[] descifrarAsimetrico(Key llave, byte[] texto){
+		byte[] textoClaro;
+		
+		try {
+			Cipher cifrador = Cipher.getInstance(ALGORITMO);
+			cifrador.init(Cipher.DECRYPT_MODE, llave);
+			textoClaro = cifrador.doFinal(texto);
+		}catch(Exception e) {
+			System.out.println("Exception: "+ e.getMessage());
+			return null;
+		}
+		return textoClaro;
+	}
+	
+	public static byte[] cifrarSimetrico(Key llave, String texto) {
+		try {
+			Cipher cifrador = Cipher.getInstance("AES");
+			cifrador.init(Cipher.ENCRYPT_MODE, llave);
+			return cifrador.doFinal(texto.getBytes());
+		} catch (Exception e) {
+			System.out.println("Exception: "+ e.getMessage());
+			return null;
+		}
+	}
+	
+	public static byte[] descifrarSimetrico(Key llave, byte[] texto) {
+		try {
+			Cipher cifrador = Cipher.getInstance("AES");
+			cifrador.init(Cipher.DECRYPT_MODE, llave);
+			return cifrador.doFinal(texto);
+		} catch (Exception e) {
+			System.out.println("Exception: "+ e.getMessage());
+			return null;
+		}
+	}
+	
+
+	
+
+
 
 }
